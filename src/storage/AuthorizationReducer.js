0b24d4287c750f6ded69f9af2244567f6dd6cf80
tato <tkats18@freeuni.edu.ge>
2022-03-10 13:23:06 +0400

    redux/routers/login

---
+++ src/storage/AuthorizationReducer.js
@@ -0,0 +1,26 @@
+const User = (state = {}, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                authorized:true,
+                userData:action.payload
+            }
+        case 'LOGOUT':
+            return {
+                ...state,
+                authorized:false,
+                userData:action.payload
+
+            }
+        default:
+            return {
+                ...state,
+                authorized:localStorage.getItem('token')!==null,
+                userData:localStorage.getItem('username')
+
+            }
+    }
+}
+
+export default User
